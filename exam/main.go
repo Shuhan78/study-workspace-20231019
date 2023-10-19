e5296ce1b0c79bc297085c33ffd428a9e855eb28
steven <shuhan99407@gmail.com>
2023-10-19 17:34:44 +0800

    creat sumUp function in folder exam at main.go

--- exam/main.go
+++ exam/main.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func hello(s string) {
-	fmt.Println("HELLO", s)
+	fmt.Println("HELLO", s, "how r u ")
+}
+
+func sumUp(x, y int) int {
+	result := x + y
+	return result
 }
 
 func main() {
 	hello("steven")
+	sumUp(2, 3)
 }
